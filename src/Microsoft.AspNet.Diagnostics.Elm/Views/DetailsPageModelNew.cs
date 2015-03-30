0ae98868a55e169cbd15a02675be95dc3298a39f
Kiran Challa <kichalla@microsoft.com>
2015-03-30 10:29:26 -0700

    updated

---
+++ src/Microsoft.AspNet.Diagnostics.Elm/Views/DetailsPageModelNew.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microsoft.AspNet.Diagnostics.Elm.Views
+{
+    public class DetailsPageModelNew
+    {
+        public ActivityContextNew Activity { get; set; }
+
+        public ViewOptions Options { get; set; }
+    }
+}
