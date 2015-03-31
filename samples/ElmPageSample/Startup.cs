29d9f16d39b2ead1ab41e1584bc949b3bc84114b
Kiran Challa <kichalla@microsoft.com>
2015-03-31 13:10:04 -0700

    Added an Elm sample

---
+++ samples/ElmPageSample/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.WebEncoders;
+
+namespace ElmPageSample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddElm();
+
+            services.ConfigureElm(elmOptions =>
+            {
+                elmOptions.Filter = (loggerName, loglevel) => true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseElmPage();
+
+            app.UseElmCapture();
+
+            app.UseMiddleware<TestMiddleware>();
+        }
+    }
+}
---
+++ samples/ElmPageSample/TestMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.Logging;
+
+namespace ElmPageSample
+{
+    public class TestMiddleware
+    {
+        private readonly ILogger _logger;
+        private readonly RequestDelegate _next;
+
+        public TestMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<TestMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            using (_logger.BeginScope("GreetingScope"))
+            {
+                _logger.LogVerbose("Getting message");
+
+                await httpContext.Response.WriteAsync("Hello World!");
+            }
+        }
+    }
+}
